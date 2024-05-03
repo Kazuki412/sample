bbe639b471c61399a2e9c3d6f8fb6d95183f28cf
Kazuki412 <153255954+Kazuki412@users.noreply.github.com>
2024-05-03 06:49:21 +0000

    投稿機能作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_03_034222) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_03_060837) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -39,6 +39,15 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_03_034222) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "post_images", force: :cascade do |t|
+    t.string "shop_name"
+    t.text "caption"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_post_images_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email", default: "", null: false
@@ -54,4 +63,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_03_034222) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "post_images", "users"
 end
